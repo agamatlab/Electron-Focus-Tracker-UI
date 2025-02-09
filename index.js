0ecce1ced56d9473ca6ba4a0bb80c4457dd5814e
agamatlab <icloudakberzade@gmail.com>
2025-02-08 21:36:06 -0500

    added: image carousel

--- index.js
+++ index.js
@@ -2,16 +2,3 @@ document.addEventListener("DOMContentLoaded", () => {
     document.body.style.userSelect = "none";
 });
 
-function toggleCarousel(){
-    const dashboard = document.getElementsByClassName("dashboard");
-    const carousel = document.getElementsByClassName("carousel");
-    if(dashboard.enabled){
-      carousel.enabled = true;
-      dashboard.enabled = false;
-    } else{
-      carousel.enabled = false;
-      dashboard.enabled = true;
-    }
-  }
-  
-  document.getElementById("viewAll")
